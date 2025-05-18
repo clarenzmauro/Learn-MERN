ffb6f78f10ea4827c2d7ebcff8e7e0fe53cb597b
clarenzmauro <m27oflegend@gmail.com>
2025-05-18 09:02:19 +0800

    javascript basics

---
+++ Week1_Essentials/Day2_JavaScript_Basics.js
@@ -0,0 +1,19 @@
+let name = "Clarenz";
+let age = 21;
+let isStudent = true;
+
+function sayHello(name) {
+    console.log("Hello, my name is " + name + "!");
+}
+
+function sayAge(age) {
+    console.log("I am " + age + " years old!");
+}
+
+function sayIsStudent(isStudent) {
+    console.log("I am a student: " + isStudent + "!");
+}
+
+sayHello(name);
+sayAge(age);
+sayIsStudent(isStudent);
---
+++ Week1_Essentials/Day2_JavaScript_Basics.md
@@ -0,0 +1,6 @@
+Variables are simply containers of information.
+
+There are different types of information.
+- String
+- Integer
+- Booleans
